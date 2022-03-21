b7a2edfe94ee16c1d262806a99dc56b1d8c4a70f
user <user@DESKTOP-6JBEF5K>
2022-03-21 10:33:49 +0800

    Httpclient

--- ConsoleApp0321/Program.cs
+++ ConsoleApp0321/Program.cs
@@ -1,18 +1,41 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ConsoleApp0321
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Car myCar = new Car();
+            /*Car myCar = new Car();
             myCar.StartEngin();
             myCar.showInfo();
             Console.WriteLine("請輸入新顏色:");
             string i = Console.ReadLine();
-            myCar.changeColor(i);
+            myCar.changeColor(i);*/
+            await HttpMain();
         }
+        static readonly HttpClient client = new HttpClient();
+        static async Task HttpMain()
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            string url = "https://data.coa.gov.tw/api/v1/CropType/?CropCode=11";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
 
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
     }
 }
